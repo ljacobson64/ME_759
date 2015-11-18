583eb273b0dc3daf00fe30a1fa8195b72bf30365
Lucas Jacobson <ljacobson64@gmail.com>
2015-11-18 15:48:04 -0600

    actually got p3a working

--- Assignment_9/src/p3.cu
+++ Assignment_9/src/p3.cu
@@ -2,13 +2,29 @@
 #include "stdlib.h"
 #include <vector>
 
+#include "thrust/count.h"
 #include "thrust/device_vector.h"
 #include "thrust/inner_product.h"
 
-int compute_number_of_days_with_rainfall(thrust::device_vector<int>& day) {
-  return thrust::inner_product(day.begin(), day.end() - 1, day.begin() + 1, 0,
-                               thrust::plus<int>(),
-                               thrust::not_equal_to<int>()) + 1;
+struct Data {
+  thrust::device_vector<int> day;
+  thrust::device_vector<int> site;
+  thrust::device_vector<int> measure;
+};
+
+int days_with_rainfall(const Data& data) {
+  return thrust::inner_product(data.day.begin(), data.day.end() - 1,
+                               data.day.begin() + 1, 1, thrust::plus<int>(),
+                               thrust::not_equal_to<int>());
+}
+
+int days_over_threshold(const Data& data, const int threshold) {
+  int N = days_with_rainfall(data);
+  thrust::device_vector<int> day(N), measure(N);
+  thrust::reduce_by_key(data.day.begin(), data.day.end(), data.measure.begin(),
+                        day.begin(), measure.begin());
+  return thrust::count_if(measure.begin(), measure.end(),
+                          thrust::placeholders::_1 > threshold);
 }
 
 int main(int argc, char** argv) {
@@ -17,16 +33,21 @@ int main(int argc, char** argv) {
   int h_site[] = {2, 3, 0, 1, 1, 2, 0, 1, 2, 1, 3, 4, 0, 1, 2};
   int h_measure[] = {9, 5, 6, 3, 3, 8, 2, 6, 5, 10, 9, 11, 8, 4, 1};
 
-  thrust::device_vector<int> d_day(N), d_site(N), d_measure(N);
+  Data data;
+  data.day.resize(N);
+  data.site.resize(N);
+  data.measure.resize(N);
 
   for (int i = 0; i < N; i++) {
-    d_day[i] = h_day[i];
-    d_site[i] = h_site[i];
-    d_measure[i] = h_measure[i];
+    data.day[i] = h_day[i];
+    data.site[i] = h_site[i];
+    data.measure[i] = h_measure[i];
   }
 
-  int number_of_days_with_rainfall = compute_number_of_days_with_rainfall(d_day);
-  printf("%d\n", number_of_days_with_rainfall);
+  int days_over_0 = days_with_rainfall(data);
+  int days_over_5 = days_over_threshold(data, 5);
+  printf("%d\n", days_over_0);
+  printf("%d\n", days_over_5);
 
   return 0;
 }
